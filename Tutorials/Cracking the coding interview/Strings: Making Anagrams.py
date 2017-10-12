ea7df16cb610773a408ee9b73259426922b68f33
zizou <zizou_vb@hotmail.com>
2017-10-12 14:30:35 +0200

    [Add] Counter for Cracking itw

--- Tutorials/Cracking the coding interview/Strings: Making Anagrams.py
+++ Tutorials/Cracking the coding interview/Strings: Making Anagrams.py
@@ -1,27 +1,13 @@
-# def number_needed(a, b):
-#     count = 0
-#     for i in range(97, 123):
-#         ia = sum(letter == chr(i) for letter in a)
-#         ib = sum(letter == chr(i) for letter in b)
-#         count += abs(ia-ib)
-#     return count
+from collections import Counter
+def ransom_note(magazine, ransom):
+    return ransom - magazine == {}
 
-def number_needed(a, b):
-    nb_to_delete = 0
-    for _ in range(len(a)):
-        letter=a[0]
-  
-        if letter in b:
-  
-            b.remove(letter)
-        else:
-  
-            nb_to_delete+=1
-        a.remove(letter)
-  
-    return nb_to_delete + len(b)
-
-a = list(input().strip())
-b = list(input().strip())
-
-print(number_needed(a, b))
+m, n = map(int, input().strip().split(' '))
+magazine = Counter(input().strip().split(' '))
+ransom = Counter(input().strip().split(' '))
+answer = ransom_note(magazine, ransom)
+if(answer):
+    print("Yes")
+else:
+    print("No")
+    
