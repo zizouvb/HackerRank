86b3dbdd0314d468d301452648e94ff68e486dc2
z <zizou_vb@hotmail.com>
2017-11-01 09:54:28 +0100

    [Add] Minimum Height math

---
+++ Mathematics/Fundamentals/Minimum Height Triangle.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import math
+
+def lowestTriangle(base, area):
+    # Complete this function
+    return math.ceil(area*2 / base)
+
+base, area = input().strip().split(' ')
+base, area = [int(base), int(area)]
+height = lowestTriangle(base, area)
+print(height)
